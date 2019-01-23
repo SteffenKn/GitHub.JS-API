58ff8583680caa360706bbb13915eca99f0a37bc
SteffenKn <Steffen.Knaup@5minds.de>
2019-01-23 23:26:26 +0100

    :white_check_mark: Add Config to Tests

--- tests/Orga.js
+++ tests/Orga.js
@@ -1,9 +1,12 @@
 'use strict';
+const config = require('./config.json');
+const Github = require('../dist/Github.js');
+
 const chai = require('chai'); 
 const expect = chai.expect;
 
-const Github = require('../dist/Github.js');
 const github = new Github.GithubApi();
+github.authToken = config.authToken;
 
 describe ('Orga Tests', () => {
   let orga;
