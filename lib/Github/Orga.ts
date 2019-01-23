6b80c1d7cc1310cafa0e501f02a8818efcb812da
SteffenKn <Steffen.Knaup@5minds.de>
2019-01-23 21:19:04 +0100

    :recycle: Refactor Orga

--- lib/Github/Orga.ts
+++ lib/Github/Orga.ts
@@ -1,17 +1,20 @@
 import {HttpClient} from '../HttpClient';
+
+import {Owner} from './abstracts/Owner';
+
 import {Repo} from './Repo';
 
-export class Orga {
-  private _httpClient: HttpClient;
-  private _name: string;
-
+export class Orga extends Owner {
   constructor(httpClient: HttpClient, name: string) {
-    this._httpClient = httpClient;
-    this._name = name;
+    super(httpClient, name);
   }
 
   public getRepo(repoName: string): Repo {
-    return new Repo(this._httpClient, this._name, repoName);
+    return new Repo(this._httpClient, this, repoName);
+  }
+
+  public _getUrl(): string {
+    return `/orgs/${this._name}/repos`;
   }
 
   public asJson(): Promise<JSON> {
