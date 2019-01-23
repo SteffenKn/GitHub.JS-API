42f656028e101355eb4ff13f616e4a99db027cda
SteffenKn <Steffen.Knaup@5minds.de>
2019-01-23 22:41:09 +0100

    :white_check_mark: Add Tests

---
+++ tests/User.js
@@ -0,0 +1,46 @@
+'use strict';
+const chai = require('chai'); 
+const expect = chai.expect;
+
+const Github = require('../dist/Github.js');
+const github = new Github.GithubApi();
+
+describe ('User Tests', () => {
+  let user;
+  let repo;
+
+  it ('Should be able to create an user', async () => {
+    user = github.getUser("octocat");
+    console.log(user);
+
+    const userData = await user.asJson();
+    const userName = userData['name'];
+
+    expect(userName).to.equal('The Octocat');
+  });
+
+  it ('Should be able to get a specific repo', async () => {
+    repo = user.getRepo("Hello-World");
+
+    const repoData = await repo.asJson();
+    const repoName = repoData['name'];
+
+    expect(repoName).to.equal('Hello-World');
+  });
+
+  it ('Should be able to get all repos', async () => {
+    const repos = await user.getAllRepos();
+    const repoNames = [];
+
+    for(const singleRepo of repos) {
+      const repoData = await singleRepo.asJson();
+      const repoName = repoData['name'];
+
+      repoNames.push(repoName);
+    }
+
+    expect(repoNames).to.contain('git-consortium');
+    expect(repoNames).to.contain('Spoon-Knife');
+    expect(repoNames).to.contain('Hello-World');
+  });
+});
