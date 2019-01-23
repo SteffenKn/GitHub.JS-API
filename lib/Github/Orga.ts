7d48ec013656989bc2ddc88717378d1b25d8202d
Steffen <steffenkna@googlemail.com>
2019-01-23 22:48:56 +0100

    :recycle: Move getRepo to Owner

--- lib/Github/Orga.ts
+++ lib/Github/Orga.ts
@@ -2,15 +2,9 @@ import {HttpClient} from '../HttpClient';
 
 import {Owner} from './abstracts/Owner';
 
-import {Repo} from './Repo';
-
 export class Orga extends Owner {
   constructor(httpClient: HttpClient, name: string) {
     super(httpClient, name);
-  }
-
-  public getRepo(repoName: string): Repo {
-    return new Repo(this._httpClient, this, repoName);
   }
 
   public _getBaseUrl(): string {
