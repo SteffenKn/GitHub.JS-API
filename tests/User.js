2a2c48b46fe9187a7cad547a45391a549a6efff2
Steffen <steffenkna@googlemail.com>
2019-01-26 16:50:33 +0100

    :white_check_mark: Add Tests for Error Handling in User and Orga

--- tests/User.js
+++ tests/User.js
@@ -21,4 +21,37 @@ describe ('User Tests', () => {
 
     expect(userName).to.equal('Steffen Knaup');
   });
+
+  it ('Should be able to get data from an user', async () => {
+    const user = github.getUser("SteffenKn");
+
+    const userData = await user.asJson();
+    const userName = userData['name'];
+
+    expect(userName).to.equal('Steffen Knaup');
+  });
+
+  it ('Should not be able to get data from an invalid user', (done) => {
+    github.getUser("Not-Existing-User").asJson()
+    .then(() => {
+      done('Did not throw an error');
+    })
+    .catch((error) => {
+      const expectedErrorMessage = 'Not Found';
+
+      const isCorrectError = error.message === expectedErrorMessage;
+      if(isCorrectError){
+        done();
+      } else {
+        done(`Wrong error was thrown. Expected: "${expectedErrorMessage}", but got "${error.message}"`);
+      }
+    });
+  });
+
+  it ('Should be able to create a repo', () => {
+    const user = github.getUser("SteffenKn");
+    const repo = user.getRepo('Cloudflare-DDNS-Sync');
+
+    expect(repo.name).to.equal('Cloudflare-DDNS-Sync');
+  });
 });
