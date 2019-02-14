17208b3857c1bd2229d752cdad274f64ed5f7540
Steffen <Steffenknaup@hotmail.de>
2019-02-14 21:30:02 +0100

    :recycle: Refactor ConfigService & HttpClient

--- lib/Github/Orga.ts
+++ lib/Github/Orga.ts
@@ -1,8 +1,11 @@
+import {ConfigService} from '../index';
+
 import {Owner} from './abstracts/Owner';
 
 export class Orga extends Owner {
-  constructor(name: string) {
-    super(name);
+
+  constructor(name: string, configService?: ConfigService) {
+    super(name, configService);
   }
 
   protected _getBaseUrl(): string {
--- lib/Github/User.ts
+++ lib/Github/User.ts
@@ -1,9 +1,11 @@
+import {ConfigService} from '../index';
+
 import {Owner} from './index';
 
 export class User extends Owner {
 
-  constructor(name: string) {
-    super(name);
+  constructor(name: string, configService?: ConfigService) {
+    super(name, configService);
   }
 
   protected _getBaseUrl(): string {
