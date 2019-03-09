8ee1a9d1a91336194d9724bda1ee2d4cf701b608
Steffen <Steffenknaup@hotmail.de>
2019-03-09 19:56:19 +0100

    :sparkles: Add asUserData

--- lib/Github/User.ts
+++ lib/Github/User.ts
@@ -1,11 +1,21 @@
 import {ConfigService} from '../index';
 
+import {createUserDataFromJson, IUserData} from '../../contracts';
+
 import {Owner} from './index';
 
 export class User extends Owner {
 
   constructor(name: string, configService?: ConfigService) {
     super(name, configService);
+  }
+
+  public async asUserData(): Promise<IUserData> {
+    const data: JSON = await super.asJson();
+
+    const userData: IUserData = createUserDataFromJson(data);
+
+    return userData;
   }
 
   protected _getBaseUrl(): string {
